a83f605cb22b1f9b7a22d51b4fbc1a1e91936362
alanh2 <alanhelouani@gmail.com>
2017-03-22 02:44:00 -0300

    Ejemplo simple de app

---
+++ music.io/app/src/main/java/com/fiuba/taller2/musicio/Login2.java
@@ -0,0 +1,13 @@
+package com.fiuba.taller2.musicio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
--- music.io/app/src/main/java/com/fiuba/taller2/musicio/LoginFacebook.java
+++ music.io/app/src/main/java/com/fiuba/taller2/musicio/LoginFacebook.java
@@ -2,12 +2,60 @@ package com.fiuba.taller2.musicio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginFacebook extends AppCompatActivity {
+    public static final String tag = "Mensaje_app";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_facebook);
+        Log.i(tag,"onCreate");
+    }
+
+    public void onSumarClick(View v){
+        EditText n1 = (EditText) findViewById(R.id.numero1);
+        EditText n2 = (EditText) findViewById(R.id.numero2);
+        TextView resultado = (TextView) findViewById(R.id.Resultado);
+        int num1 = Integer.parseInt(n1.getText().toString());
+        int num2 = Integer.parseInt(n2.getText().toString());
+
+        int suma = num1 + num2;
+        resultado.setText(Integer.toString(suma));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(tag,"onStart");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(tag,"onResume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(tag,"onPause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(tag,"onStop");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(tag,"onRestart");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(tag,"onDestroy");
     }
 }
